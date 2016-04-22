59106f7404a6a7b36bf11d4383414ff43692fe56
Steven Bougon <sbougon@salesforce.com>
2016-04-22 16:06:28 +0200

    Added a new query param flag: networkRequestsOnTimeline
    This flag will enable and select the network option in the timeline panel

---
+++ chrome-devtools-frontend/front_end/controller.js
@@ -0,0 +1,14 @@
+console.log("Patching Runtime.experiments.supportEnabled ...");
+// hack to enable experiments: see Runtime.js:Runtime.isEnabled()
+Runtime.experiments.supportEnabled = () => true;
+
+if (Runtime.queryParam('networkRequestsOnTimeline')) {
+    console.log("Patching localStorage.experiments.networkRequestsOnTimeline to show the network pane ...");
+    var experiments = JSON.parse(window.localStorage['experiments'] || "{}");
+    experiments.networkRequestsOnTimeline = true;
+    window.localStorage['experiments'] = JSON.stringify(experiments);
+    console.log("Patching Runtime.experiments.localStorage to select the network pane ...");
+    window.localStorage["timelineCaptureNetwork"] = true
+}
+
+console.log("Hello, your controller ready to server you");
