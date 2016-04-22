2300b57bf610cfc416ec58b7d2bf96081e9c80b9
Steven Bougon <sbougon@salesforce.com>
2016-04-22 16:11:13 +0200

    Clear storage everytime as the URL is the unique state driver

--- chrome-devtools-frontend/front_end/controller.js
+++ chrome-devtools-frontend/front_end/controller.js
@@ -2,6 +2,8 @@ console.log("Patching Runtime.experiments.supportEnabled ...");
 // hack to enable experiments: see Runtime.js:Runtime.isEnabled()
 Runtime.experiments.supportEnabled = () => true;
 
+clearLocalStorage();
+
 if (Runtime.queryParam('networkRequestsOnTimeline')) {
     console.log("Patching localStorage.experiments.networkRequestsOnTimeline to show the network pane ...");
     var experiments = JSON.parse(window.localStorage['experiments'] || "{}");
@@ -12,3 +14,11 @@ if (Runtime.queryParam('networkRequestsOnTimeline')) {
 }
 
 console.log("Hello, your controller ready to server you");
+
+
+/*
+* no persistence, the URL drives everything
+*/
+function clearLocalStorage () {
+    window.localStorage.clear();
+}
